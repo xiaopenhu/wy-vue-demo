6eddadc3e2aae1ae3c379172b884693fdabb27a5
yylovely <17637000021@163.com>
2020-07-12 08:32:07 +0800

    v-if、v-else-if、v-else 指令

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
-    <p v-bind:id="pid">动态属性 id</p>
-    <p :id="pid">简写形式</p>
-    <p :class="{ a: isA, b: isB }">使用 class</p>
-    <p :class="[black, yellow]">使用 class （数组）</p>
-    <p :style="styleData">使用 style</p>
+    <p v-if="type === 'a'">A</p>
+    <p v-else-if="type === 'b'">B</p>
+    <p v-else-if="type === 'c'">C</p>
+    <p v-else>other</p>
   </div>
 </template>
 <script>
 export default {
   data () {
     return {
-      pid: 'pid',
-      isA: true,
-      isB: true,
-
-      black: 'black1',
-      yellow: 'yellow1',
-
-      styleData: {
-        fontSize: '40px', // 转换为驼峰式
-        color: 'red',
-        backgroundColor: '#ccc' // 转换为驼峰式
-      }
+      type: 'a'
     }
   }
 }
