fcdf911806d8d268a84c06879007f99c881d80e1
yylovely <17637000021@163.com>
2020-07-12 08:19:58 +0800

    v-bind/class/style

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,31 @@
 <template>
   <div id="app">
-    <p v-text="msgText"></p>
-    <p v-html="msgHtml"></p>
+    <p v-bind:id="pid">动态属性 id</p>
+    <p :id="pid">简写形式</p>
+    <p :class="{ a: isA, b: isB }">使用 class</p>
+    <p :class="[black, yellow]">使用 class （数组）</p>
+    <p :style="styleData">使用 style</p>
   </div>
 </template>
 <script>
 export default {
   data () {
     return {
-      msgText: '<span style="color:red">我是v-text</span>',
-      msgHtml: '<span style="color:red">我是v-html</span>'
+      pid: 'pid',
+      isA: true,
+      isB: true,
+
+      black: 'black1',
+      yellow: 'yellow1',
+
+      styleData: {
+        fontSize: '40px', // 转换为驼峰式
+        color: 'red',
+        backgroundColor: '#ccc' // 转换为驼峰式
+      }
     }
   }
 }
 </script>
 <style>
-  [v-cloak] {
-    display: none;
-  }
 </style>
