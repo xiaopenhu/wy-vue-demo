16b19042ba3be97208f23168ee26da8651e0ee0f
yylovely <17637000021@163.com>
2020-07-12 09:15:01 +0800

    v-for循环数组和对象

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,34 @@
 <template>
   <div id="app">
-    <p v-if="type === 'a'">A</p>
-    <p v-else-if="type === 'b'">B</p>
-    <p v-else-if="type === 'c'">C</p>
-    <p v-else>other</p>
+    <p>遍历数组</p>
+    <ul>
+      <li v-for="(item, index) in listArr" :key="item.id">
+        {{index}} - {{item.id}} - {{item.title}}
+      </li>
+    </ul>
+
+    <p>遍历对象</p>
+    <ul >
+      <li v-for="(val, key) in listObj" :key="key">
+        {{key}} -  {{val.title}}
+      </li>
+    </ul>
   </div>
 </template>
 <script>
 export default {
   data () {
     return {
-      type: 'a'
+      listArr: [
+        { id: 'a', title: '标题1' }, // 数据结构中，最好有 id ，方便使用 key
+        { id: 'b', title: '标题2' },
+        { id: 'c', title: '标题3' }
+      ],
+      listObj: {
+        a: { title: '标题1' },
+        b: { title: '标题2' },
+        c: { title: '标题3' }
+      }
     }
   }
 }
