f9aee995d2ca78e10bd56b05ab8cfae466761176
王雨 <17637000021@163.com>
2020-07-13 16:38:07 +0800

    'props-父组件向子组件传值'

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,18 @@
 <template>
   <div id="app">
-    <p>修改触发更新过程：<input type="text" v-model="name"></p>
-    <LifeChild :name="name"></LifeChild>
-    <div>
-      <button @click="changeState('A')">A</button>
-      <button @click="changeState('B')">B</button>
-      <button @click="changeState('C')">C</button>
-      <keep-alive> <!-- tab 切换 -->
-        <KeepAliveStateA v-if="state === 'A'"/> <!-- v-show -->
-        <KeepAliveStateB v-if="state === 'B'"/>
-        <KeepAliveStateC v-if="state === 'C'"/>
-      </keep-alive>
-    </div>
+    <LifeChild :name="msg"></LifeChild>
   </div>
 </template>
 <script>
 import LifeChild from './components/LifeChild'
-import KeepAliveStateA from './components/KeepAliveStateA'
-import KeepAliveStateB from './components/KeepAliveStateB'
-import KeepAliveStateC from './components/KeepAliveStateC'
 export default {
   components: {
-    LifeChild,
-    KeepAliveStateA,
-    KeepAliveStateB,
-    KeepAliveStateC
+    LifeChild
   },
   data () {
     return {
-      name: '前端爆米花',
-      state: 'A'
+      msg: '我是父组件的值，现在传递给了子组件'
     }
-  },
-  methods: {
-    changeState (state) {
-      this.state = state
-    }
-  },
-  beforeCreate () {
-    console.log('父组件-beforeCreate')
-  },
-  created () {
-    console.log('父组件-created')
-  },
-  beforeMount () {
-    console.log('父组件-beforeMount')
-  },
-  mounted () {
-    console.log('父组件-mounted')
-  },
-  beforeUpdate () {
-    console.log('父组件-beforeUpdate')
-  },
-  updated () {
-    console.log('父组件-updated')
-  },
-  beforeDestroy () {
-    console.log('父组件-beforeDestroy')
-  },
-  destroyed () {
-    console.log('父组件-destroyed')
-  },
-  activated () {
-    console.log('父组件-activated')
-  },
-  deactivated () {
-    console.log('父组件-deactivated')
   }
 }
 </script>
--- src/components/LifeChild.vue
+++ src/components/LifeChild.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <p>父子生命周期：<input type="text" :value="name"></p>
+        <p>{{name}}</p>
     </div>
 </template>
 
@@ -8,36 +8,6 @@
 
 export default {
   name: 'LifeChild',
-  props: ['name'],
-  beforeCreate () {
-    console.log('子组件-beforeCreate')
-  },
-  created () {
-    console.log('子组件-created')
-  },
-  beforeMount () {
-    console.log('子组件-beforeMount')
-  },
-  mounted () {
-    console.log('子组件-mounted')
-  },
-  beforeUpdate () {
-    console.log('子组件-beforeUpdate')
-  },
-  updated () {
-    console.log('子组件-updated')
-  },
-  beforeDestroy () {
-    console.log('子组件-beforeDestroy')
-  },
-  destroyed () {
-    console.log('子组件-destroyed')
-  },
-  activated () {
-    console.log('子组件-activated')
-  },
-  deactivated () {
-    console.log('子组件-deactivated')
-  }
+  props: ['name']
 }
 </script>
