1375f8c8328645888526e2aeb42bbb4701245ce5
王雨 <17637000021@163.com>
2020-07-22 13:47:40 +0800

    '过滤器'

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <template>
   <div id="app">
-    <p>路径about和about1在同一模块异步加载，路径pay时另一模块异步加载</p>
-    <p>路径user同步加载，可加参数</p>
-    <p>我是路由参数：{{$route.params.id}}</p>
-    <!-- 路由链接 -->
-    <router-link to="/">我是路由链接</router-link>
-    <!-- 路由占位符 -->
-    <router-view/>
+    <h1>过滤器</h1>
+    <input type="text" v-model='msg'>
+    <div>{{msg | upper}}</div>
+    <div>{{msg | upper | lower}}</div>
+    <div>{{msg | upper | lower2('abc')}}</div>
+    <div :abc='msg | upper'>动态属性应用</div>
   </div>
 </template>
 <script>
 export default {
-  mounted () {
-    console.log(this.$route.params.id)
+  data: function () {
+    return {
+      msg: 'aaaa'
+    }
+  },
+  filters: {
+    upper: function (val) {
+      return val.charAt(0).toUpperCase() + val.slice(1)
+    },
+    lower: function (val) {
+      return val.charAt(0).toLowerCase() + val.slice(1)
+    },
+    lower2: function (val, arg) {
+      return val.charAt(0).toLowerCase() + val.slice(1) + arg
+    }
   }
 }
 </script>
